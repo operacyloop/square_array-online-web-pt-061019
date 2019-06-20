064a160b18cc5afd30baa26d821f61c7731abbd4
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-20 20:49:18 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+ m
 # Square Array
 
 ## Objectives
--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,16 @@
-#def square_array(array)
-#  arr = []
-#  array.each { |i| arr << i ** 2 }
-#  arr
+# def square_array(array)
+# arr = []
+# array.each { |i| arr << i ** 2 }
+# arr
 # end
 # puts square_array([1, 2, 3]) 
+ 
+ def square_array(array)
+ new_array = []
+ array.each { |array_element| new_array << array_element * array_element }
+ new_array
+ end
+ puts square_array([1, 2, 3]) 
 
 # def square_array(array)
 #   array == array.each do |number|
@@ -12,5 +19,10 @@
 #     end
 # end
 
-array = [1, 2, 3]
-array.map = {| n*n |}
+# def square_array(whatarethenumbers)
+#   return whatarethenumbers * whatarethenumbers
+#   puts whatarethenumbers * whatarethenumbers
+# end
+
+# square_array([1, 2, 3])
+# square_array([9,10,16,25])
