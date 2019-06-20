b84dcecd83583ba355b99120808f4aa697af3f90
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-20 03:02:36 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
-end
+#def square_array(array)
+#  arr = []
+#  array.each { |i| arr << i ** 2 }
+#  arr
+# end
+# puts square_array([1, 2, 3]) 
+
+# def square_array(array)
+#   array == array.each do |number|
+#   square_of_the_array = number * number
+#   return square_of_the_array
+#     end
+# end
+
+array = [1, 2, 3]
+array.map = {| n*n |}
